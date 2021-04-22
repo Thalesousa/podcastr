f88d241e6f8abad32458595a66c48f3fade96b54
thalesousa <thalestjsb@gmail.com>
2021-04-22 18:06:25 -0300

    Aula 04 - Landing ✅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+
 import { format, parseISO } from 'date-fns';
 import  ptBR  from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../context/PlayerContext';
+
 import styles from './home.module.scss';
 
 type Episode = {
@@ -24,6 +29,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return(
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -50,7 +57,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <time>{episode.durationAsString}</time>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
